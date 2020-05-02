51b7da0b4623211e4fa379e88b04bd8297169481
naseemali925 <naseem.ali.925@gmail.com>
2020-05-02 22:09:05 +0530

    Removed Unused Code

--- src/app/pages/job/list.tsx
+++ src/app/pages/job/list.tsx
@@ -14,14 +14,16 @@ import React, { useState } from 'react'
 import Job from '../../../components/job'
 import PadColContainer from 'components/layout/pad-col-container'
 import styles from './style.module.less'
+import { Row } from 'antd'
 
 // Dummy Data
 const job = {
 	'@context': 'https://schema.org/',
 	'@type': 'JobPosting',
 	title: 'Software Engineer',
-	description:
-		'<p>Google aspires to be an organization that reflects the globally diverse audience that our products and technology serve. We believe that in addition to hiring the best talent, a diversity of perspectives, ideas and cultures leads to the creation of better products and services.</p>',
+	description: `<p>Google aspires to be an organization that reflects the globally diverse audience.
+		, a diversity of perspectives, ideas and cultures leads to the creation of better products and services. 
+		</p>`,
 	identifier: {
 		'@type': 'PropertyValue',
 		name: 'Google',
@@ -65,35 +67,15 @@ for (let i = 1; i < 25; i++) {
 }
 
 const JobComponent: React.FC = () => {
-	const [data, setListData] = useState(listData)
-	const [loading, setLoading] = useState(false)
-	const [hasMore, setHasMore] = useState(true)
+	const [data] = useState(listData)
 	const [active, setActive] = useState(0)
-
-	const handleInfiniteOnLoad = () => {
-		setLoading(true)
-
-		// To check if all items are loaded
-		// if (data.length >= 23) {
-		//     message.warning('Infinite List loaded all');
-		// setLoading(false);
-		// setHasMore(false);
-		//     return;
-		// }
-
-		// Do Data Fetching then setLoading false
-		setLoading(false)
-		setHasMore(false)
-	}
-
-	const handleListItemClick = (key) => {
-		setActive(key)
-	}
 
 	const JobList = () => (
 		<div className={styles['job-list']}>
 			{data.map((job, key) => (
-				<Job key={key} item={job} active={active === key} />
+				<Row key={key} onClick={() => setActive(key)}>
+					<Job key={key} item={job} active={active === key} />
+				</Row>
 			))}
 		</div>
 	)
